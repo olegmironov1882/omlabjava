510f8eff205cc3a1016abd915aed9e9950ff9263
oleg.mironov1882 <oleg.mironov1882@gmail.com>
2020-05-12 08:13:36 -0400

    New Changes

--- App01.java
+++ App01.java
@@ -8,6 +8,6 @@ package com.omlab;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This will be printed");
+        System.out.println("Hello to everyone");
     }
 }
