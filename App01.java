73cae20e05e5432076d5bc0210c1bd47b7ffc26b
oleg.mironov1882 <oleg.mironov1882@gmail.com>
2020-05-12 07:41:03 -0400

    First commit

---
+++ App01.java
@@ -0,0 +1,13 @@
+package com.omlab;
+
+
+/**
+ * 
+ * Hello World
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+    }
+}
